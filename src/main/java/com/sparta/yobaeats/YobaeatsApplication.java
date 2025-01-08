b857348bb58e73639d159e550b0d2fb721baba24
SeuneKeun <eiziong1@gmail.com>
2025-01-08 09:37:51 +0900

    feat: set up project

---
+++ src/main/java/com/sparta/yobaeats/YobaeatsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.yobaeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobaeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YobaeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/yobaeats/YobaeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.yobaeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobaeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
